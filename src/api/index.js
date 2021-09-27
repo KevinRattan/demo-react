b8a3ca857949a6ad041635d0bf8f66e4c86e27bc
Kevin Rattan <kevin@time2yak.com>
2021-09-27 06:50:32 -0400

    simplified

--- src/api/index.js
+++ src/api/index.js
@@ -1,9 +1,4 @@
-// This env var is stored in .env and is set at build time
-// it cannot be passed in to the container via yaml, as react produces static content
-// edit the .env file to set the value to http://IP.Address/ - where IP.Address
-// is the  external IP of your API service
-// NB. Your service type will need to be LoadBalancer, not ClusterIP, if you are using AJAX
-const url = process.env.REACT_APP_SERVER;
+const url = "http://localhost:8082/";
 
 const headers = {
     accept: 'application/json',
